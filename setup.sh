806a6a67f63655ca299d56e88d479ba78c65e2b1
Ludvig Lifting <9506luli@gmail.com>
2022-01-20 11:12:53 +0100

    lolxd

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#Read pi number for connecting via ssh
+echo "Please provide pi-nbr"
+read pinbr
+address="inutiuser@inuti$pinbr"
+
+#Transfer pub key
+scp ~/.ssh/id_rsa.pub "$address:~/temp"
+
+#start ssh sessions
+ssh $address < "LangGeNot5G"
+
+##Authorize key
+cat ~/temp >> .ssh/authorized_keys
+
+#Configurate git
+git config user.name $pinbr
+git config user.email "lolxd@lolxd.com"
+
+#clone repo
+mkdir ~/git
+cd ~/git
+git clone https://github.com/emilpedersenlundh/eitn30-project.git
+
+#fix permissions
+chmod -R u+x eitn30-project/
+
+#Download python libraries
+python3 -m pip install --upgrade pip setuptools
