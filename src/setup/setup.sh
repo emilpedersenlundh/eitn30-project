ae7ebbf086e6baa35c34b006281392e28f02b5cd
InternetInutiPi10 <notan@email.com>
2022-01-28 16:05:10 +0000

    Pre Live Test
    Setup split into SSH-authorize, and local setup.

--- src/setup/setup.sh
+++ src/setup/setup.sh
@@ -1,67 +1,49 @@
 #!/usr/bin/env bash
 
-#Read pi number for connecting via ssh
-echo "Please provide pi-nbr"
-read pinbr
-address="inutiuser@inuti$pinbr.lab.eit.lth.se"
-repository="https://github.com/emilpedersenlundh/eitn30-project.git"
-repofolder="eitn30-project"
-password="LangGeNot5G"
-
 ROOT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $ROOT_PATH
 cd ..
-pwd
 
-##BUG: Connecting via SSH works differently than what was assumed when writing the code below. SSH has to be passed a script in order to run it.
-#Transfer pub key
-scp ~/.ssh/id_rsa.pub "$address:~/temp-key"
-
-#start ssh sessions
-ssh $address < $password
-
-#Authorize key
-cat ~/temp-key >> ~/.ssh/authorized_keys
-rm ~/temp-key
+password="LangGeNot5G"
 
 #Configure git
-git config user.name "InternetInutiPi$pinbr"
-git config user.email "notan@email.com"
+git config --global user.name "InternetInutiPi$pinbr"
+git config --global user.email "notan@email.com"
 
 #Git repository installation
-if [[ -f "~/git/" ]]
+if [[ -f "$HOME/git/" ]]
 then
-    if [[ -f "~/git/eitn30-project/" ]]
+    if [[ -f "$HOME/git/$repofolder/" ]]
     then
         echo "Git repository exists, updating."
-        cd ~/git/eitn30-project/
+        cd $HOME/git/$repofolder/
         git pull
     else
         echo "Git repository missing, cloning."
-        cd ~/git/
+        cd $HOME/git/
         git clone $repository
     fi
 else
     echo "Git directory missing. Creating and cloning repository."
-    mkdir ~/git/
-    cd ~/git/
+    mkdir $HOME/git/
+    cd $HOME/git/
     git clone $repository
 fi
 
 #Fix permissions
-cd ~/git/
-chmod -R u+x eitn30-project/
-cd ~/git/eitn30-project/
+cd $HOME/git/
+chmod -R u+x $repofolder/
+cd $HOME/git/$repofolder/
 echo "Repository permissions set."
 
-#Install radio dependencies
-##C++ Library
-##Check if exists, else install
-if [[ -f "~/usr/include/RF24/" ]]
+##Install radio dependencies
+#C++ Library
+if [[ -f "/usr/include/RF24/" ]]
 then
     echo "LibRF24 exists on system. Skipping installation."
 else
     # Download latest release
+    echo "Installing LibRF24."
     curl -s https://api.github.com/repos/nRF24/RF24/releases/latest \
     | grep "browser_download_url.*deb" \
     | grep arm64 \
@@ -69,12 +51,12 @@ else
     | cut -d '"' -f 4 \
     | wget -qi -
     # Install .deb
-    sudo dpkg -i ~/git/eitn30-project/librf24-RPi*arm64.deb
-    rm ~/git/eitn30-project/librf24-RPi*arm64.deb
+    sudo dpkg -i $HOME/git/$repofolder/librf24-RPi*arm64.deb
+    rm $HOME/git/$repofolder/librf24-RPi*arm64.deb
     echo "LibRF24 installed."
 fi
 
-sudo apt update
+echo $password | sudo -S apt update
 sudo apt-get install -y python3-dev libboost-python-dev python3-pip python3-rpi.gpio build-essentials
 sudo ln -s $(ls /usr/lib/$(ls /usr/lib/gcc | tail -1)/libboost_python3*.so | tail -1) /usr/lib/$(ls /usr/lib/gcc | tail -1)/libboost_python3.so
 
@@ -82,27 +64,29 @@ sudo ln -s $(ls /usr/lib/$(ls /usr/lib/gcc | tail -1)/libboost_python3*.so | tai
 python3 -m pip install --upgrade pip setuptools
 
 #Build LibRF24 Python wrapper
-cd ~/git/$repofolder/
+echo "Building pyRF24 wrapper."
+cd $HOME/git/$repofolder/
 git clone https://github.com/nRF24/RF24.git
-cd ~/git/$repofolder/RF24/pyRF24/
+cd $HOME/git/$repofolder/RF24/pyRF24/
 python3 setup.py build
+echo "Build complete."
 
 #Install and activate python virtual environment
-if [[ -f "~/.envs/$repofolder/" ]]
+if [[ -f "$HOME/.envs/$repofolder/" ]]
 then
     echo "Python venv exists. Switching to environment."
-    source ~/.envs/$repofolder/bin/activate
+    source $HOME/.envs/$repofolder/bin/activate
 else
-    if [[ -f "~/.envs/" ]]
+    if [[ -f "$HOME/.envs/" ]]
     then
         echo "Python venv does not exist. Creating and activating."
-        python3 -m venv ~/.envs/$repofolder
-        source ~/.envs/$repofolder/bin/activate
+        python3 -m venv $HOME/.envs/$repofolder
+        source $HOME/.envs/$repofolder/bin/activate
     else
         echo "Python venv does not exist. Creating and activating."
-        mkdir ~/.envs/
-        python3 -m venv ~/.envs/$repofolder
-        source ~/.envs/$repofolder/bin/activate
+        mkdir $HOME/.envs/
+        python3 -m venv $HOME/.envs/$repofolder
+        source $HOME/.envs/$repofolder/bin/activate
     fi
 fi
 
@@ -110,8 +94,10 @@ fi
 python3 -m pip install --upgrade pip setuptools
 
 #Install LibRF24 Python wrapper
-cd ~/git/$repofolder/RF24/pyRF24/
+echo "Installing pyRF24 wrapper."
+cd $HOME/git/$repofolder/RF24/pyRF24/
 python3 setup.py install
+echo "Installation complete."
 
-#Setup virtual interface
+##Setup virtual interface
 #modprobe tun
