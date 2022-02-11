a35d41c75178cf6636831ba22ee7a889516790a6
InternetInutiPi9 <notan@email.com>
2022-02-11 15:44:38 +0000

    Socket Start + App dependencies + Setup

---
+++ src/server.py
@@ -0,0 +1,11 @@
+#!/home/fideloper/.envs/eitn30-project/bin/python3
+
+from multiprocessing import Process
+import socket
+
+INTERFACE = ''
+PORT=5000
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.bind((INTERFACE,PORT))
+    s.listen(1)
