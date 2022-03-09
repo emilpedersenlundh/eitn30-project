4cc42dfa41199244f7b7af8e5cdd02aed204dde5
InternetInutiPi6 <notan@email.com>
2022-03-09 16:21:59 +0000

    Server constructor + interface

--- src/server.py
+++ src/server.py
@@ -1,31 +1,46 @@
 #!/home/fideloper/.envs/eitn30-project/bin/python3
 
+from ast import Not
 import os
 import fcntl
 import struct
-from multiprocessing import Process
-import socket
+import typing
+from multiprocessing import Process, Queue
 import scapy.all as scapy
 from tuntap import TunTap
 
 class Server:
 
-    def __init__(self) -> None:
+    def __init__(self, ip):
         # Local variables
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.tun = Interface(nic_type='tun', nic_name='longge', ip_address=ip, mask='255.255.255.0', gateway='10.10.10.1')
 
-    def open_socket(self, interface, port):
-        # Check input parameters
-        self.s.bind((interface, port))
-        self.s.listen(1)
+    def read(self):
+        self.tun.read()
 
-    def __create_tun(self, ip_address):
-        # Create and configure a TUN interface
-        tun = TunTap(nic_type="Tun", nic_name="longge")
-        tun.config(ip=ip_address, mask="255.255.255.0", gateway="10.10.10.1")
-        # Read from TUN interface
-        #buf = tun.read(size)
-        # Write to TUN interface
-        #tun.write(buf)
-        # Close and destroy interface
-        tun.close()
+    def write(self, buffer):
+        for i in buffer:
+            try:
+                data = buffer[i].pop(0)
+                if data != None:
+                    self.tun.write(data)
+            except Exception as e:
+                print(e)
+
+class Interface:
+
+    def __init__(self, nic_type, nic_name, ip_address, mask, gateway):
+        self.tun = TunTap(nic_type, nic_name)
+        self.tun.ip = ip_address
+        self.tun.mask = mask
+        self.tun.gateway = gateway
+        self.tun.mtu = 1500
+
+    def __del__(self):
+        self.tun.close()
+
+    def read(self):
+        self.tun.read(self.tun.mtu)
+
+    def write(self, buffer):
+        self.tun.write(buffer)
