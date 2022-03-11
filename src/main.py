d258d1fdd3e07544dd19d45a42ea4ec75c30e530
InternetInutiPi6 <notan@email.com>
2022-03-11 15:18:32 +0000

    Added demonstratory loop

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 #!/home/fideloper/.envs/eitn30-project/bin/python3
 
-
+from time import sleep
 
 # Project packages
 from server import Server as server
 
 
-
 if __name__ == "__main__":
     s = server('longge')
+    s.set_ip('10.10.10.1')
+
+    while True:
+        try:
+            print(s.ip)
+            sleep(0.5)
+
+        except KeyboardInterrupt:
+            print("\nKeyboard Interrupt\n")
+            exit()
