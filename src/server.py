41a792f78730e3a7fdad58b9bd864a1d21016dcc
InternetInutiPi6 <notan@email.com>
2022-03-11 15:17:46 +0000

    Server interface creation + configuration

--- src/server.py
+++ src/server.py
@@ -3,27 +3,41 @@
 import os
 import fcntl
 import struct
+import subprocess
 
 class Server:
 
     def __init__(self, iface):
 
+        self.iface = iface
+        self.ip = 'default'
         self.tun = Interface(iface)
 
     def read(self):
         self.tun.read()
 
     def write(self, buffer):
+        """
+        Writes to Tun interface. If successful returns true, else false.
+        """
         for i in buffer:
             try:
                 data = buffer[i].pop(0)
                 if data != None:
-                    self.tun.write(data)
+                    written = self.tun.write(data)
+                return written
             except Exception as e:
                 print(e)
 
-    def set_ip():
-        pass
+    def set_ip(self, ip):
+        """
+        Sets IP of the Tun interface. If successful returns true, else false.
+        """
+        self.ip = ip
+        self.tun.interface_default_settings(ip)
+        #if str(subprocess.check_call(check, shell=True)) != ip:
+        #    return False
+        #return True
 
 class Interface:
 
@@ -33,13 +47,17 @@ class Interface:
         TUNSETOWNER = TUNSETIFF + 2
         IFF_TUN = 0x0001
         IFF_NO_PI = 0x1000
-        self.mtu = 2048
+        self.iface = iface
+        self.mtu = 1500
 
         # Opens already existing TUN interface
         self.tun = open('/dev/net/tun', 'r+b', 0)
         ifr= struct.pack('16sH', bytes(iface,'utf-8'), IFF_TUN | IFF_NO_PI)
         fcntl.ioctl(self.tun, TUNSETIFF, ifr)
         fcntl.ioctl(self.tun, TUNSETOWNER, 1000)
+
+        # Sets default values for TUN interface
+        self.interface_default_settings('10.10.10.1')
 
     def read(self):
         packet = os.read(self.tun.fileno(), self.mtu)
@@ -50,3 +68,12 @@ class Interface:
         if written != 0:
             return True
         return False
+
+    def interface_default_settings(self, ip):
+        cmd_netmask = 'netmask 255.255.255.0'
+        cmd_broadcast = 'broadcast 10.10.10.255'
+        cmd_mtu = "mtu {}".format(self.mtu)
+        cmd = "ifconfig {} {} {} {} {}".format(self.iface, ip, cmd_netmask, cmd_broadcast, cmd_mtu)
+        subprocess.check_call(cmd, shell=True)
+    def __routing_init():
+        pass
