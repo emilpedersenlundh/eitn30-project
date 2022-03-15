81227df42d6f02aa8ea7933bf0b94b7f7edf2335
InternetInutiPi6 <notan@email.com>
2022-03-15 09:16:36 +0000

    Adjusted write() to accept list of queues

--- src/server.py
+++ src/server.py
@@ -1,10 +1,12 @@
 #!/home/fideloper/.envs/eitn30-project/bin/python3
 
+from asyncio import QueueEmpty
 import os
 import fcntl
 import struct
 import random
 import subprocess
+from multiprocessing import Queue
 
 class Server:
 
@@ -22,29 +24,25 @@ class Server:
         """
         self.tun.read()
 
-    def write(self, buffer):
+    def write(self, buffer: list[Queue]):
         """
         Writes to TUN interface. If successful returns true, else false.
         """
+        #Expect buffer[i, q: Queue]
         for i in buffer:
             try:
-                data = buffer[i].pop(0)
-                if data != None:
-                    written = self.tun.write(data)
+                data = buffer[i].get(False)
+                written = self.tun.write(data)
                 return written
-            except Exception as e:
-                print(e)
+            except QueueEmpty as e:
+                print('Buffer pipe {}: \n{}'.format(i, e))
 
     def set_ip(self, ip):
         """
-        Sets IP of the Tun interface. If successful returns true, else false.
+        Sets IP of the Tun interface.
         """
-        self.ip = ip
-        #self.tun.set_interface(ip)
         self.tun.set_ip(ip)
-        #if str(subprocess.check_call(check, shell=True)) != ip:
-        #    return False
-        #return True
+        self.ip = ip
 
 class Interface:
 
@@ -66,7 +64,7 @@ class Interface:
         fcntl.ioctl(self.tun, TUNSETOWNER, 1000)
 
         # Sets default values for TUN interface
-        self.set_interface(self.ip)
+        self.__set_interface(self.ip)
 
     def __delete__(self):
         self.tun.close()
@@ -81,7 +79,18 @@ class Interface:
             return True
         return False
 
-    def set_interface(self, ip):
+    def set_ip(self, ip):
+        """
+        Sets the IP of the server and TUN interface.
+        """
+        cmd_remove = "ip addr del {}/24 dev {}".format(self.ip, self.iface)
+        cmd_add = "ip addr add {}/24 dev {}".format(ip, self.iface)
+        subprocess.check_call(cmd_remove, shell=True)
+        subprocess.check_call(cmd_add, shell=True)
+        self.ip = ip
+        self.__routing_init()
+
+    def __set_interface(self, ip):
         """
         Applies settings to the TUN interface.
         """
@@ -94,27 +103,21 @@ class Interface:
         subprocess.check_call(cmd_up, shell=True)
         self.ip = ip
 
-    def set_ip(self, ip):
-        """
-        Sets the IP of the server and TUN interface.
-        """
-        cmd_remove = "ip addr del {}/24 dev {}".format(self.ip, self.iface)
-        cmd_add = "ip addr add {}/24 dev {}".format(ip, self.iface)
-        subprocess.check_call(cmd_remove, shell=True)
-        subprocess.check_call(cmd_add, shell=True)
-        self.ip = ip
-
     def __routing_init(self):
-        #TODO: Apply routing rules. Depend on parameter 'mode'.
         """
         Applies routing rules depending on operating mode.
         """
-        cmd_base = ' \ip route add 10.10.10.0/24 via {} dev {}'.format(self.ip, self.iface)
-        cmd_node = ''
+        cmd_base_a = 'iptables -t nat -A POSTROUTING -o {} -j MASQUERADE'.format(self.iface)
+        cmd_base_b = 'iptables -A FORWARD -i eth0 -o {} -m state --state RELATED,ESTABLISHED -j ACCEPT'.format(self.iface)
+        cmd_base_c = 'iptables -A FORWARD -i {} -o eth0 -j ACCEPT'.format(self.iface)
+
+        cmd_node = 'ip route add default via {} dev {}'.format(self.ip, self.iface)
 
         if self.mode == 'BASE':
             try:
-                subprocess.check_call(cmd_base, shell=True)
+                subprocess.check_call(cmd_base_a, shell=True)
+                subprocess.check_call(cmd_base_b, shell=True)
+                subprocess.check_call(cmd_base_c, shell=True)
             except subprocess.CalledProcessError as e:
                 print(e.output)
         else:
