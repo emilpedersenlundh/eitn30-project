b9ae159927d177be615efcae6dcbe97e8243481a
InternetInutiPi6 <notan@email.com>
2022-04-13 14:32:00 +0100

    benchmark rewrite

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,10 +1,9 @@
 #!/home/fideloper/.envs/eitn30-project/bin/python3
 
-import os
-import sys
-from time import sleep, time
+from time import sleep, time_ns
 from multiprocessing import Process, Queue
-import numpy as np
+from numpy import random
+from matplotlib import pyplot as plt
 import pandas as pd
 
 # Project packages
@@ -20,33 +19,65 @@ PIPE_ADDRESSES = [
     b"\xC2\xC2\xC2\xC2\xC5",
     b"\xC2\xC2\xC2\xC2\xC6",
 ]
-
-latency: list[int] = []
-outcome: list[bool] = []
-queue_size: list[int] = []
+X_RESOLUTION: int = 200  # Determines the amount of x axis data points, and the input rate denominator (1/X_RESOLUTION seconds)
 
 
-def add(queue):
-    """Adds elements to a provided queue."""
-    measurement_count: int = 100
-    data = np.random.bytes(30)
-
-    for i in range(measurement_count):
-        queue.put(data)
-
-
-def transmit(queue):
+def transmit(data: Queue, measurement_count=100, resolution=X_RESOLUTION):
     """Transmits elements from the provided queue via radio."""
     r = radio("NODE")
+    plot_data = []
 
-    if not queue.empty():
-        outcome.append(r.transmit(PIPE_ADDRESSES[1], queue.get()))
+    for i in range(resolution):
+        plot_data.append(_measure(r, data, measurement_count))
+
+
+def input(data_queue: Queue, resolution=X_RESOLUTION):
+    """Inputs data elements into provided queue."""
+    for i in range(1, resolution + 1):
+        _add(data_queue, element_count=100, interval=(1 / i))
+
+
+def _add(queue: Queue, element_count: int, interval: float):
+    """Adds element_count random byte elements to a provided queue with an interval of interval seconds."""
+    data = random.bytes(30)
+
+    for i in range(element_count):
+        queue.put(data)
+        sleep(interval)
+
+
+def _measure(
+    radio: radio,
+    data_queue: Queue,
+    measurement_count: int,
+) -> list[list]:
+    "Returns outcome and execution time for the transmit, as well as input queue size."
+
+    latency: list[int] = []
+    outcome: list[bool] = []
+    queue_size: list[int] = []
+    time_start: int
+    time_end: int
+
+    for i in range(measurement_count):
+        queue_size.append(data_queue.qsize())
+
+        data = data_queue.get(block=True, timeout=5)
+
+        time_start = time_ns()
+        outcome.append(radio.transmit(PIPE_ADDRESSES[1], data))
+        time_end = time_ns()
+        latency.append(time_start - time_end)
+    return [outcome, latency, queue_size]
+
+
+def plot(plot_data, x_resolution=X_RESOLUTION):
+    pass
 
 
 def run_test(queue):
-
     rx = Process(target=transmit, args=queue)
-    tx = Process(target=add, args=queue)
+    tx = Process(target=input, args=queue)
 
     rx.start()
     sleep(1)
